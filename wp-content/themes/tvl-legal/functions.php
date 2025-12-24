53c4de90cb7f62717a4bcf8093e78b7d407e2cdc
Đặng Thanh Phong <dangphong.it@gmail.com>
2025-12-24 10:12:08 +0700

    up code

--- wp-content/themes/tvl-legal/functions.php
+++ wp-content/themes/tvl-legal/functions.php
@@ -1218,6 +1218,7 @@ function tvl_register_practice_area_taxonomy() {
         'show_ui' => true,
         'show_admin_column' => true,
         'show_in_nav_menus' => true,
+        'show_in_rest' => true,
         'show_tagcloud' => false,
         'rewrite' => array('slug' => 'linh-vuc'),
     );
--- wp-content/themes/tvl-legal/single.php
+++ wp-content/themes/tvl-legal/single.php
@@ -5,6 +5,11 @@ if ( have_posts() ) :
         <div class="container my-5">
             <h1><?php the_title(); ?></h1>
             <p class="text-muted"><?php echo get_the_date(); ?></p>
+            <?php if (has_post_thumbnail()) : ?>
+                <div class="post-thumbnail mb-4">
+                    <?php the_post_thumbnail('large', array('class' => 'img-fluid rounded')); ?>
+                </div>
+            <?php endif; ?>
             <div><?php the_content(); ?></div>
         </div>
     <?php endwhile;
